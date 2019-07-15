c0a49038a004597c4b0426cd1a510180926faf83
Alexey Gulev <Alexey.Gulev@king.com>
2019-07-15 12:48:41 +0200

    Fixed conflict with other NE that uses FileProvider

---
+++ share/src/SharingFileProvider.java
@@ -0,0 +1,9 @@
+package com.britzl.defold.sharing;
+
+import android.support.v4.content.FileProvider;
+
+public class SharingFileProvider extends FileProvider
+{
+//Empty class Only for manifest.
+//It helps to avoid conflicts in merging tool
+}
