89aa46618eada6f63562dc60a03e2f72913de1a8
Alexey Gulev <me@agulev.com>
2020-12-17 16:37:11 +0100

    Updated for using AndroidX

--- share/src/SharingFileProvider.java
+++ share/src/SharingFileProvider.java
@@ -1,6 +1,6 @@
 package com.britzl.defold.sharing;
 
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 
 public class SharingFileProvider extends FileProvider
 {
